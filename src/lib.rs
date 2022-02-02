74b1ff672b8f2f2797ad501efb51fd54bba501fd
= <alexandru.brg@gmail.com>
2022-02-02 15:15:53 +0200

    Print matched lines and highlight the query

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use colored::*;
 use std::error::Error;
 use std::fs;
 
@@ -19,10 +20,45 @@ impl Config {
   }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+fn print_found_line(line: &str, query: &str) {
+  let line_to_print = line.replace(query, &query.red().to_string());
+  println!("{}", line_to_print);
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.filename)?;
 
-  println!("With text:\n{}", contents);
+  for line in search(&config.query, &contents) {
+    print_found_line(line, &config.query);
+  }
 
   Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
 }
